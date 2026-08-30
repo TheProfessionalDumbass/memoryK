80d3fc1544c7dd2595959b5429689cb88315dc30
grammarr_issu <93713321+TheProfessionalDumbass@users.noreply.github.com>
2026-08-30 08:49:33 +0800

    Update comm.h

--- kernel/comm.h
+++ kernel/comm.h
@@ -1,20 +1,36 @@
 #ifndef MEMKERNEL_COMM_H
 #define MEMKERNEL_COMM_H
 
-struct CopyMemory { pid_t pid; uintptr_t addr; void *buffer; size_t size; };
-struct ModuleBase { pid_t pid; char *name; uintptr_t base; };
+#include <linux/types.h>
+#include <linux/pid.h>
 
+// Structure for reading/writing memory
+struct CopyMemory {
+    pid_t pid;
+    uintptr_t addr;
+    void *buffer;
+    size_t size;
+};
 
+// Structure for getting module base
+struct ModuleBase {
+    pid_t pid;
+    char *name;
+    uintptr_t base;
+};
+
+// NEW: Structure for Aimbot delta (sent from userspace)
 struct AimDelta {
     int dx;
     int dy;
 };
 
-enum Operations { 
-    OP_READ_MEM = 0x801, 
-    OP_WRITE_MEM = 0x802, 
+// IOCTL Commands
+enum Operations {
+    OP_READ_MEM = 0x801,
+    OP_WRITE_MEM = 0x802,
     OP_MODULE_BASE = 0x803,
-    OP_AIM_MOVE = 0x804,  
+    OP_AIM_MOVE = 0x804,   // NEW: Inject touch delta
 };
 
 #endif
